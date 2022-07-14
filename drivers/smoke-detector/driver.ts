f1d374edd41bfeeb567ff61c182448b0f635eefb
Rhuan Barreto <rhuan.barreto@cognite.com>
2022-07-14 23:56:14 +0200

    add smoke detector

---
+++ drivers/smoke-detector/driver.ts
@@ -0,0 +1,47 @@
+import {
+  BearerTokenAuthenticator,
+  SmartThingsClient,
+} from "@smartthings/core-sdk";
+import Homey from "homey";
+import PairSession from "homey/lib/PairSession";
+
+class MyDriver extends Homey.Driver {
+  async onInit() {
+    this.log("MyDriver has been initialized");
+  }
+
+  async onPair(session: PairSession) {
+    let token = "";
+
+    session.setHandler("login", async (data: { password: string }) => {
+      token = data.password;
+      const client = new SmartThingsClient(new BearerTokenAuthenticator(token));
+      const credentialsAreValid = await client.locations.list();
+      this.log(credentialsAreValid);
+      return Boolean(credentialsAreValid);
+    });
+
+    session.setHandler("list_devices", async () => {
+      const client = new SmartThingsClient(new BearerTokenAuthenticator(token));
+      const myDevices = await client.devices.list({
+        capability: "smokeDetector",
+      });
+
+      const devices = myDevices.map((device) => {
+        return {
+          name: device.label ?? device.name,
+          data: {
+            id: device.deviceId,
+          },
+          settings: {
+            password: token,
+          },
+        };
+      });
+
+      return devices;
+    });
+  }
+}
+
+module.exports = MyDriver;
