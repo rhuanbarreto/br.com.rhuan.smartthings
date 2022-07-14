503e7e644a2c2473d29fd5d20c5a10f5355daf03
Rhuan Barreto <rhuan.barreto@cognite.com>
2022-07-14 23:56:03 +0200

    cleanup lock driver

--- drivers/lock/driver.ts
+++ drivers/lock/driver.ts
@@ -5,31 +5,9 @@ import {
 import Homey from "homey";
 import { PairSession } from "homey/lib/Driver";
 
-class MyDriver extends Homey.Driver {
-  /**
-   * onInit is called when the driver is initialized.
-   */
+class LockDriver extends Homey.Driver {
   async onInit() {
     this.log("MyDriver has been initialized");
-  }
-
-  /**
-   * onPairListDevices is called when a user is adding a device and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
-   */
-  async onPairListDevices() {
-    return [
-      // Example device data, note that `store` is optional
-      // {
-      //   name: 'My Device',
-      //   data: {
-      //     id: 'my-device',
-      //   },
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      // },
-    ];
   }
 
   async onPair(session: PairSession) {
@@ -64,4 +42,4 @@ class MyDriver extends Homey.Driver {
   }
 }
 
-module.exports = MyDriver;
+module.exports = LockDriver;
